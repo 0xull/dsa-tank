fcb1393fd3eb9e10098081f07f2d8b4a6f57f2e8
IkehAkinyemi <mrikehchukwuka@gmail.com>
2025-05-28 09:39:32 +0100

    feat(stack-array): add Push, Pop & Peek implementation for the array based stack

--- ds/stack/stack.go
+++ ds/stack/stack.go
@@ -1,23 +1,55 @@
 package stackarray
 
+import (
+	"fmt"
+)
+
 // StackArray is an array based implementation of the stack ADT
 type StackArray struct {
 	items []any
 	top int
-	max uint
+	max int
 }
 
 // NewStackArray initializes and returns a pointer instance of StackArray
-func NewStackArray(capacity uint) *StackArray {
-	if capacity == 0 {
+func NewStackArray(capacity int) *StackArray {
+	if capacity <= 0 {
 		capacity = 10
 	}
 	
 	return &StackArray{
 		items: make([]any, capacity),
 		top: -1,
-		max: capacity
+		max: capacity,
 	}
 }
 
+// Push adds a new item to the top of the stack.
+func (stack *StackArray) Push(value any) error {
+	if stack.top >= (stack.max - 1) {
+		return fmt.Errorf("stack overflow")
+	}
+	stack.top++
+	stack.items[stack.top] = value
+	return nil
+}
 
+// Pop removes and returns the item at the top of the stack.
+func (stack *StackArray) Pop() (any, error) {
+	if stack.top == -1 {
+		return nil, fmt.Errorf("stack underflow")
+	}
+	item := stack.items[stack.top]
+	stack.items[stack.top] = nil
+	stack.top--
+	return item, nil
+}
+
+// Peek returns the item at the top of the stack without removing it.
+func (stack *StackArray) Peek() any {
+	if stack.top == -1 {
+		return nil
+	}
+	
+	return stack.items[stack.top]
+}
