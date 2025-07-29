3dfa887149dee7ffa310d8b72933db6013d4ec19
0xull <mrikehchukwuka@gmail.com>
2025-07-29 14:18:45 +0100

    feat(redblack-tree): implemented the insertion logic, which is a basic binary search tree insertion with red black colors ordering adhering to redblack tree rules

--- tree/redblack_tree.go
+++ tree/redblack_tree.go
@@ -17,6 +17,40 @@ type RBNode[T cmp.Ordered] struct {
 	Parent *RBNode[T]
 }
 
-type RBTree[T cmp.Ordered] struct {
+type RedBlackTree[T cmp.Ordered] struct {
 	root *RBNode[T]
+}
+
+func (rbt *RedBlackTree[T]) Insert(val T) {
+	nn := &RBNode[T]{Value: val, Color: RED}
+	
+	if rbt.root == nil {
+		rbt.root = nn
+	} else {
+		curr := rbt.root
+		var parent *RBNode[T]
+		for curr != nil {
+			parent = curr
+			if val < curr.Value {
+				curr = curr.Left
+			} else if val > curr.Value {
+				curr = curr.Right
+			} else {
+				return
+			}
+		}
+		
+		nn.Parent = parent
+		if val < parent.Value {
+			parent.Left = nn
+		} else {
+			parent.Right = nn
+		}
+	}
+	
+	rbt.fixInsert(nn)
+}
+
+func (rbt *RedBlackTree[T]) fixInsert(z *RBNode[T]) {
+	
 }
