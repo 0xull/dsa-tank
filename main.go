6bcae2925f55444ddb1dd5fc165e539937f41e0c
0xull <mrikehchukwuka@gmail.com>
2025-08-01 19:32:20 +0100

    style: setting second workspace on dell xps 13

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	sll := &linkedlist.SinglyLinkedList[Process]{
 		Head: &linkedlist.Node[Process]{Data: Process{1}, Next: &linkedlist.Node[Process]{Data: Process{2}, Next: nil}},
 	}
-	
+
 	sll.Display()
 	fmt.Println(sll.Search(Process{2}))
 	sll.Prepend(Process{0})
@@ -27,46 +27,45 @@ func main() {
 	fmt.Printf("%v\n", sll.DeleteTail())
 	fmt.Printf("%v\n", sll.Delete(Process{2}))
 	sll.Display()
-	
+
 	cll := &linkedlist.CircularLinkedList[Process]{}
 	cll.Prepend(Process{0})
 	cll.Append(Process{1})
 	cll.Prepend(Process{-1})
 	cll.Append(Process{2})
-	
+
 	cll.Display()
 	fmt.Println(cll.Delete(Process{2}))
-	
+
 	dll := &linkedlist.DoublyLinkedList[Process]{}
-	
+
 	dll.Prepend(Process{0})
 	dll.Display()
-	
 
 	origInt := []int{1, 2, 4, 5}
 	reversed := stack.ReverseArray(origInt)
 	fmt.Println(reversed)
-	
+
 	origStr := []rune{'s', 't', 'r', 'i', 'n', 'g', 's'}
 	reversed1 := stack.ReverseArray(origStr)
 	fmt.Println(string(reversed1))
-	
+
 	fmt.Println("--- Parentheses Checker ---")
 
 	expressions := []string{
-		"{([])}",          // Balanced
-		"([{}])()",        // Balanced
-		"((()))",          // Balanced
+		"{([])}",                        // Balanced
+		"([{}])()",                      // Balanced
+		"((()))",                        // Balanced
 		"var x = func({a:1, b:[2,3]});", // Balanced (ignores non-parentheses)
-		"((()",            // Unbalanced (too many open)
-		"())",             // Unbalanced (closing before open)
-		"{[}]",            // Unbalanced (mismatched and wrong order)
-		"({[}])",          // Unbalanced (mismatched order)
-		"([)]",            // Unbalanced (mismatched order)
-		"",                // Balanced (empty string)
-		"abc",             // Balanced (no parentheses)
-		"[",               // Unbalanced
-		"}",               // Unbalanced
+		"((()",                          // Unbalanced (too many open)
+		"())",                           // Unbalanced (closing before open)
+		"{[}]",                          // Unbalanced (mismatched and wrong order)
+		"({[}])",                        // Unbalanced (mismatched order)
+		"([)]",                          // Unbalanced (mismatched order)
+		"",                              // Balanced (empty string)
+		"abc",                           // Balanced (no parentheses)
+		"[",                             // Unbalanced
+		"}",                             // Unbalanced
 	}
 
 	for _, expr := range expressions {
@@ -78,7 +77,7 @@ func main() {
 	}
 	expression := "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )"
 	fmt.Println(stack.EvalParenthesizedInfixExpression(expression))
-	
+
 	fmt.Println(shuntingyard.ShuntingYard(expression))
 	// expression = "3 * ( 4 + 2 ) - 6 / 3"
 	expression = "5+((1+2)*4)-3"
