f81eff7b87e0e3967f54208ebdbf7a36e78dc68d
0xull <mrikehchukwuka@gmail.com>
2025-08-06 11:26:10 +0100

    feat: added the complete rotation (left and right) for the fixInsertion function on insert ops

--- tree/redblack_tree.go
+++ tree/redblack_tree.go
@@ -49,9 +49,9 @@ func (rbt *RedBlackTree[T]) Insert(val T) {
 			parent.Right = nn
 		}
 	}
-	
+			
 	rbt.fixInsert(nn)
-}
+} 
 
 func (rbt *RedBlackTree[T]) fixInsert(z *RBNode[T]) {
 	for z != rbt.root && z.Parent.Color == RED {
@@ -102,5 +102,44 @@ func (rbt *RedBlackTree[T]) fixInsert(z *RBNode[T]) {
 	rbt.root.Color = BLACK
 }
 
-func (rbt *RedBlackTree[T]) leftRotate(node *RBNode[T]) {}
-func (rbt *RedBlackTree[T]) rightRotate(node *RBNode[T]) {}
+func (rbt *RedBlackTree[T]) leftRotate(x *RBNode[T]) {
+	y := x.Right
+	x.Right = y.Left
+	
+	if y.Left != nil {
+		x.Right.Parent = x
+	}
+	
+	y.Parent = x.Parent
+	if x.Parent == nil {
+		rbt.root = y
+	} else if x == x.Parent.Left {
+		x.Parent.Left = y
+	} else {
+		x.Parent.Right = y
+	}
+	
+	y.Left = x
+	x.Parent = y
+}
+
+func (rbt *RedBlackTree[T]) rightRotate(y *RBNode[T]) {
+	x := y.Left
+	y.Left = x.Right
+	
+	if x.Right != nil {
+		y.Left.Parent = y
+	}
+	
+	x.Parent = y.Parent
+	if y.Parent == nil {
+		rbt.root = x
+	} else if y == y.Parent.Left {
+		y.Parent.Left = x
+	} else {
+		y.Parent.Right = x
+	}
+	
+	x.Right = y
+	y.Parent = x
+}
