d39cf8c794948ef759d9481b88a6b724e27955a8
0xull <mrikehchukwuka@gmail.com>
2025-08-15 12:12:01 +0100

    feat(heapsort): still fleshing the understanding of the siftdown logic

---
+++ tree/algo/heapsort.go
@@ -0,0 +1,29 @@
+package bt_algo
+
+import "cmp"
+
+func heapsort[T cmp.Ordered](slice []T) {
+	n := len(slice)
+	for i := (n / 2) - 1; i >= 0; i-- {
+		siftDown(slice, n, i)
+	}
+}
+
+func siftDown[T cmp.Ordered](slice []T, n, i int) {
+	largest := i
+	left := 2*i + 1
+	right := 2*i + 2
+	
+	if left < n && slice[left] > slice[largest] {
+		largest = left
+	}
+	
+	if right < n && slice[right] > slice[largest] {
+		largest = right
+	}
+	
+	if largest != i {
+		slice[i], slice[largest] = slice[largest], slice[i]
+		siftDown(slice, n, largest)
+	}
+}
