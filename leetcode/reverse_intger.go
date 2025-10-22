4f7715245d646efbca8ac19637570b2d976edf93
0xull <mrikehchukwuka@gmail.com>
2025-10-22 10:39:51 +0100

    refactor(leetcode-medium): Reverse the integer, with constraint that the integer is within the bound of int32
    Still exist a suspected redundancy with the place-value initial value

--- leetcode/reverse_intger.go
+++ leetcode/reverse_intger.go
@@ -1,45 +1,60 @@
 package leetcode
 
 import (
-	"testing"
 	"math"
+	"testing"
 )
 
 func reverse(x int) int {
 	if x > -10 && x < 10 {
-        return x
-    }
+		return x
+	}
 
-    placeVal := 0
-    isPos := true
-    rev := 0
+	placeVal := 1
+	isPos := true
+	rev := 0
 
-    if x < 0 {
-        isPos = false
-        x *= -1
-    }
+	if x < 0 {
+		isPos = false
+		x *= -1
+	}
 
-    for math.Pow10(placeVal) < float64(x) {
-        placeVal++
-    }
+	for math.Pow10(placeVal) <= float64(x) {
+		placeVal++
+	}
 
-    for i := placeVal; i >= 0; i-- {
-        rev += (x % 10) * int(math.Pow10(i))
-        x /= 10
-    }
-    
-    if rev < math.MinInt32 || rev > math.MaxInt32 {
-        return 0
-    }
+	for i := placeVal-1; i >= 0; i-- {
+		rev += (x % 10) * int(math.Pow10(i))
+		x /= 10
+	}
 
+	if rev < math.MinInt32 || rev > math.MaxInt32 {
+		return 0
+	}
 
-    if isPos {
-        return rev
-    }
+	if isPos {
+		return rev
+	}
 
-    return rev * -1
+	return rev * -1
 }
 
 func TestReverseInt(t *testing.T) {
+	testCases := []struct {
+		name   string
+		args   int
+		expect int
+	}{{"[REVERSE INTEGER] first test", 123, 321},
+		{"[REVERSE INTEGER] second test", -120, -21},
+		{"[REVERSE INTEGER] third test", 20, 2},
+	}
 	
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := reverse(tc.args)
+			if got != tc.expect {
+				t.Errorf("got %d; expected %d", got, tc.expect)
+			}
+		})
+	}
 }
